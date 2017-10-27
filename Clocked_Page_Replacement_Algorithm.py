cc4978cd10410919745b4aa7f22c26c73cd3aa6d
ABDUL HASIB <abdulhasibuddin@gmail.com>
2017-10-27 22:22:39 +0600

    Update Clocked_Page_Replacement_Algorithm.py

--- Clocked_Page_Replacement_Algorithm.py
+++ Clocked_Page_Replacement_Algorithm.py
@@ -1,16 +1,12 @@
 #Clocked Page Replacement Algorithm::
+
 def main():
-    pages = raw_input("Pages: ")
+    pages = raw_input("Processes: ")
     page_list = map(int, pages.split(" "))
-
-    R_bits = raw_input("Reference-bits: ")
-    R_bit_list = map(int, R_bits.split(" "))
-
-    print "Pages: ",page_list
-    print "Reference bit:",R_bit_list
+    print "Required process list=",page_list
 
     no_frames = input("No. of frames: ")
-    index = page_hit = hit_count = page_fault = temp = isEmpty = lowest_weight = lowest_weight_pointer = 0
+    index = page_hit = hit_count = page_fault = temp = isEmpty = 0
 
     frame_list = [""]
     frame_R_list = [""]
@@ -24,18 +20,18 @@ def main():
         for j in range(0,len(frame_list)):
 
             if(frame_list[j] == page_list[i]):
-                frame_R_list[j] = 1
+                print "Page hit for process ", page_list[i]
                 page_hit = 1
                 hit_count += 1
-                print "block 1: frame_list=",frame_list,"; frame_R_list",frame_R_list
+                print "frame_list=",frame_list,"\n"
                 break
 
             elif(frame_list[j] == ""):
                 frame_list[j] = page_list[i]
-                frame_R_list[j] = R_bit_list[i]
+                print "Process ",page_list[i],"is placed on frame position ",j
                 page_fault += 1
                 isEmpty = 1
-                print "block 2: frame_list=",frame_list,"; frame_R_list",frame_R_list
+                print "frame_list=",frame_list,"\n"
                 break
 
         if(page_hit == 1):
@@ -45,20 +41,30 @@ def main():
             isEmpty = 0
             continue
 
-        for j in range(0, len(frame_list)):
-            if(index > len(frame_list)-1):
+        print "Reference-bits for", frame_list, " = "
+        R_bits = raw_input() #input reference bits for each corresponding process
+        frame_R_list = map(int, R_bits.split(" ")) #splitting list
+        print "Current reference-bits for frame processes =",frame_R_list
+
+        index = 0 #ensuring the search starts from a fixed frame position
+        for j in range(0, len(frame_list)+1):
+            if(index > len(frame_list)-1): #ensuring circular move through frame list
                 index = 0
+
             if(frame_R_list[index] == 0):
                 frame_list[index] = page_list[i]
-                frame_R_list[index] = R_bit_list[i]
-                print "block 3: frame_list=",frame_list,"; frame_R_list",frame_R_list
+                print "\nUpdated frame_list=",frame_list,"\n"
                 page_fault += 1
                 index += 1
                 break
             elif(frame_R_list[index] == 1):
                 frame_R_list[index] = 0
+                print "Reference bit for existing process ",frame_list[j]," is converted to 0. Updated reference list=",frame_R_list,". Moving to next frame position..."
                 index += 1
-                print "block 4: frame_list=",frame_list,"; frame_R_list",frame_R_list
+                print "frame_list=",frame_list,"\n"
+
+            if(j == len(frame_list)-1):
+                j = 0
 
     print "page_fault=",page_fault,"; hit_count=",hit_count
 
